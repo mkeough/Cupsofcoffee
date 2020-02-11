7b8cc5a14f6fcb9a31ddb7db3a44aaee5c2bcda4
Michael Keough <keoughm21@gmail.com>
2020-02-11 09:31:28 -0500

    'created project!'

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Cupsofcoffee
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      var numberOfCupsOfCoffee = "3";
+
+      String fullName = "Michael Keough";
+
+      var today = "02 / 10 / 2020";
+
+
+
+
+      Console.WriteLine(fullName + " " + today + " " + numberOfCupsOfCoffee);
+
+      //Ask user what name is
+      Console.WriteLine("What is your name?");
+      // user responds
+      string v1 = Console.ReadLine();
+      var user = v1;
+      //Greet user
+      Console.WriteLine("Welcome, " + user);
+      //ask user to pick a number between 1-25
+      Console.WriteLine("Please pick a number between 1-25");
+      Operand1 = Console.ReadLine(firstNumber);
+      Console.WriteLine(Please pick a number between 25 - 50);
+      operand2 = Console.ReadLine(secondNumber);
+      sum var = (firstNumber + secondNumber);
+      difference var = (secondNumber - firstNumber);
+      quotient var = (secondNumber / firstNumber);
+      product var = (firstNumber * secondNumber);
+
+
+
+
+
+
+
+
+
+    }
+  }
+}
